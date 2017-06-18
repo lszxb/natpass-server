f6be7b3e1dff05b4573ca0267349abef08d5df36
lszxb <798971857.ab@gmail.com>
2017-06-18 14:17:24 +0800

    Fix bugs

--- natpassserver.py
+++ natpassserver.py
@@ -36,20 +36,21 @@ while True:
             data = 'wait\n' + clientList[response[-1]][4]
             s.sendto(data.encode('utf-8'), addr)
             clientList[response[1]][3] = SENDING
-            data = 'send\n' + addr[0] + '\n' + str(addr[1]) + clientList[response[1]][4]
+            data = 'send\n' + addr[0] + '\n' + str(addr[1]) + '\n' + clientList[response[1]][4]
             s.sendto(data.encode('utf-8'), (clientList[response[1]][0], clientList[response[1]][1]))
         else:
             clientList[response[-1]] = [addr[0], addr[1], response[1], WAITING, str(random.randint(1, 10000))]
             data = 'wait\n' + clientList[response[-1]][4]
             s.sendto(data.encode('utf-8'), addr)
     elif response[0] == 'done':
-        if clientList[responese[-1]][3] == SENDING:
-            clientList[responese[-1]][3] = SENDED
-            temp = clientList[responese[-1]][2]
+        if clientList[response[-1]][3] == SENDING:
+            clientList[response[-1]][3] = SENDED
+            temp = clientList[response[-1]][2]
             clientList[temp][3] = RESENDING
             data = 'send\n' + addr[0] + '\n' + str(addr[1]) + '\n' +clientList[temp][4]
             s.sendto(data.encode('utf-8'), (clientList[temp][0], clientList[temp][1]))
-        elif clientList[responese[-1]][3] == RESENDING:
-            clientList[responese[-1]][3] = SENDED
+        elif clientList[response[-1]][3] == RESENDING:
+            clientList[response[-1]][3] = SENDED
     elif response[0] == 'success':
         clientList.pop(clientList.pop(response[-1])[2])
+        print('End')
